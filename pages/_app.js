20f540eedce84629ba6a003faf07b87f1d188a51
zhzlatkov <zhzlatkov@gmail.com>
2023-06-20 23:43:27 +0300

    Setup Font Awesome Icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
