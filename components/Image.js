db0678c033712ddb9636957efd4073f97154df5d
zhzlatkov <zhzlatkov@gmail.com>
2023-06-22 22:20:50 +0300

    fix: updating Image component with option to exclude the container

--- components/Image.js
+++ components/Image.js
@@ -2,14 +2,17 @@ import React from "react";
 import NextImage from "next/image";
 
 export default function Image({
+  withContainer = true,
   classNameRoot = "",
   classNameImage = "",
   src,
   alt = "",
 }) {
-  return (
+  return withContainer ? (
     <div className={`${classNameRoot} relative`}>
       <NextImage fill className={classNameImage} src={src} alt={alt} />
     </div>
+  ) : (
+    <NextImage fill className={classNameImage} src={src} alt={alt} />
   );
 }
