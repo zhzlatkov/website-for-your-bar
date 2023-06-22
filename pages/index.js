83fca730c54c3ca6a8b76daa4c0b18ecc78afb6e
zhzlatkov <zhzlatkov@gmail.com>
2023-06-22 22:50:07 +0300

    feat: Services component, with cover photo,  key services

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar.js";
+import Services from "@/components/Services.js";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Services />
     </>
   );
 }
