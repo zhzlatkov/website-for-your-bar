f27a55753c5fccb57679a5d831dfb4066d8ca0c5
zhzlatkov <zhzlatkov@gmail.com>
2023-06-23 23:47:44 +0300

    feat: Address component and empty Menu component
    the Menu component is added to follow the normal Home page structure

---
+++ components/Menu.js
@@ -0,0 +1,3 @@
+export default function Menu() {
+  return <div className="mx-auto max-w-7xl py-10 px-6"></div>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
+import Menu from "@/components/Menu.js";
 import Navbar from "../components/Navbar.js";
+import Address from "@/components/Address.js";
 import Services from "@/components/Services.js";
 import Separator from "@/components/Separator.js";
+
 export default function Home() {
   return (
     <>
       <Navbar />
       <Services />
       <Separator />
+      <Menu />
+      <Separator />
+      <Address />
     </>
   );
 }
