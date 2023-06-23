691b1aafa50aa35870276434fca936c861cb9acb
zhzlatkov <zhzlatkov@gmail.com>
2023-06-23 23:27:02 +0300

    fix: add max width to the navbar and additional margin to it

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,27 +7,29 @@ import {
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between bg-gradient-to-r from-yellow-400 via-white to-yellow-400 h-12 items-center">
-      <Link
-        href="mailto:zhzlatko@gmail.com"
-        alt="send email to zhzlatkov@gmail.com"
-        className="ml-4"
-      >
-        <EnvelopeOpenIcon className="text-white w-8 h-8 mx-auto"></EnvelopeOpenIcon>
-      </Link>
-      <Image
-        src="https://cdn.pixabay.com/photo/2017/09/23/21/21/label-2780146_1280.png"
-        alt="Bar logo"
-        classNameRoot="w-20 h-20 rounded-full border-8 border-white z-10 mt-10"
-        classNameImage=" "
-      ></Image>
-      <Link
-        href="tel:+359892461022"
-        alt="call to telephone number :+359892461022"
-        className="mr-4"
-      >
-        <PhoneArrowUpRightIcon className="text-white w-8 h-8 mx-auto"></PhoneArrowUpRightIcon>
-      </Link>
+    <nav className="bg-gradient-to-r from-yellow-400 via-white to-yellow-400 ">
+      <div className="flex justify-between h-12 items-center m-auto max-w-7xl">
+        <Link
+          href="mailto:zhzlatko@gmail.com"
+          alt="send email to zhzlatkov@gmail.com"
+          className="ml-16"
+        >
+          <EnvelopeOpenIcon className="text-white w-8 h-8 mx-auto"></EnvelopeOpenIcon>
+        </Link>
+        <Image
+          src="https://cdn.pixabay.com/photo/2017/09/23/21/21/label-2780146_1280.png"
+          alt="Bar logo"
+          classNameRoot="w-20 h-20 rounded-full border-8 border-white z-10 mt-10"
+          classNameImage=" "
+        ></Image>
+        <Link
+          href="tel:+359892461022"
+          alt="call to telephone number :+359892461022"
+          className="mr-16"
+        >
+          <PhoneArrowUpRightIcon className="text-white w-8 h-8 mx-auto"></PhoneArrowUpRightIcon>
+        </Link>
+      </div>
     </nav>
   );
 }
