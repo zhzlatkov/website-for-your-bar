5c89698fb7867a554d3c6c85ab17863a2d7bdda7
zhzlatkov <zhzlatkov@gmail.com>
2023-07-02 18:13:03 +0300

    feat: Component ProductsTable and admin page with all products

---
+++ components/ProductsTable.js
@@ -0,0 +1,150 @@
+import Link from "next/link";
+import Image from "./Image";
+
+export default function ProductsTable({ products }) {
+  return (
+    <div className="pt-8 px-4 sm:px-6 lg:px-8 bg-white">
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-xl font-semibold text-gray-900">Products</h1>
+          <p className="mt-2 text-sm text-gray-700">
+            A list of all the products in our web-platform including their id,
+            name, category, prices and availability.
+          </p>
+        </div>
+        <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+          <Link href="create-product">
+            <button
+              type="button"
+              className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+            >
+              Add new product
+            </button>
+          </Link>
+        </div>
+      </div>
+      <div className="mt-8 flex flex-col">
+        <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-300">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                    >
+                      ID
+                    </th>
+                    <th
+                      scope="col"
+                      className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      Price
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      Quantity
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      Category
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      Status
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      Photo
+                    </th>
+                    <th
+                      scope="col"
+                      className="relative py-3.5 pl-3 pr-4 sm:pr-6"
+                    >
+                      <span className="sr-only">Edit</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200 bg-white">
+                  {products.map((product) => (
+                    <tr key={product.id}>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        <div className="text-gray-500">{product.id}</div>
+                      </td>
+                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                        <div className="flex items-center">
+                          <div className="h-10 w-10 flex-shrink-0">
+                            <Image
+                              classNameRoot="h-10 w-10 rounded-full"
+                              classNameImage="object-cover"
+                              src={product.photoUrl}
+                              alt="Image"
+                            />
+                          </div>
+                          <div className="ml-4 font-medium text-gray-900">
+                            {product.name}
+                          </div>
+                        </div>
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        <div className="text-gray-500">{product.price}</div>
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        <div className="text-gray-500">
+                          {product.quantity} {product.quantityType}
+                        </div>
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        <div className="text-gray-500">{product.category}</div>
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        {product.status ? (
+                          <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
+                            Available
+                          </span>
+                        ) : (
+                          <span className="inline-flex rounded-full bg-red-100 px-2 text-xs font-semibold leading-5 text-red-800">
+                            Not Available
+                          </span>
+                        )}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        <Link className="text-gray-500">
+                          {product.photoPath}
+                        </Link>
+                      </td>
+                      <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                        <Link
+                          href={`edit-product/${product.id}`}
+                          className="text-indigo-600 hover:text-indigo-900"
+                        >
+                          Edit
+                          <span className="sr-only">, {product.name}</span>
+                        </Link>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ pages/your-bar-admin/products.js
@@ -0,0 +1,24 @@
+import AdminLayout from "@/components/AdminLayout";
+import prisma from "../../services/prismaClient.mjs";
+import ProductsTable from "@/components/ProductsTable";
+
+export default function Products({ products }) {
+  return (
+    <AdminLayout current={"products"}>
+      <ProductsTable products={products}></ProductsTable>
+    </AdminLayout>
+  );
+}
+
+export async function getServerSideProps() {
+  let products = await prisma.Products.findMany();
+  products = products.map((p) => ({
+    ...p,
+  }));
+
+  return {
+    props: {
+      products,
+    },
+  };
+}
