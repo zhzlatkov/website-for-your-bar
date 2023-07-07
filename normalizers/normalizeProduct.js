d753dca3d4f744761bf7b0118cae1f4716a7b05a
zhzlatkov <zhzlatkov@gmail.com>
2023-07-07 23:09:25 +0300

    feat: Prodcut normalizer and validator

---
+++ normalizers/normalizeProduct.js
@@ -0,0 +1,23 @@
+export default function normalizeProduct(product) {
+  let sanitizedProduct = {
+    product: {
+      name: String(product.product.name).trim().toLowerCase(),
+      price: Number(product.product.price),
+      quantity: Number(product.product.quantity),
+      quantityType: String(product.product.quantityType).trim().toLowerCase(),
+      shortDescription: String(product.product.shortDescription).trim(),
+      longDescription: String(product.product.longDescription).trim(),
+      status: Boolean(product.product.status),
+      photoPath: String(product.product.photoPath).trim().toLowerCase(),
+    },
+    categoryId: Number(product.brandId),
+    image: product.image,
+  };
+  if (product.image) {
+    sanitizedProduct.image = String(product.image).trim();
+  }
+  if (product.product.id) {
+    sanitizedProduct.product.id = Number(product.product.id);
+  }
+  return sanitizedProduct;
+}
---
+++ validators/productValidator.js
@@ -0,0 +1,92 @@
+import { object, string, boolean, number } from "yup";
+import prisma from "../services/prismaClient.mjs";
+
+async function validateProduct(product) {
+  let productSchema = object({
+    product: object({
+      id: number(),
+      name: string().required(),
+      price: number().required().positive().integer(),
+      quantity: number().required().positive().integer(),
+      quantityType: string(),
+      shortDescription: string().required(),
+      longDescription: string().required(),
+      status: boolean().required(),
+      photoPath: string().when("uploadPhoto", {
+        is: (val) => !val || val.length === 0,
+        then: string().required(
+          "photo path is required if there is no uploaded photo"
+        ),
+        otherwise: string(),
+      }),
+    }),
+    categoryId: number().required().positive().integer(),
+    uploadPhoto: string().when("photoPath", {
+      is: (val) => !val || val.length === 0,
+      then: string().required(
+        "uploading photo is required when creating a product"
+      ),
+      otherwise: string(),
+    }),
+  });
+  let isProductValid = {};
+
+  try {
+    productSchema.validate(product);
+    isProductValid = { result: true };
+  } catch (err) {
+    isProductValid = { result: false, err };
+  }
+
+  return isProductValid;
+}
+
+export default async function productValidator(sanitizedProduct) {
+  const isProductValid = await validateProduct(sanitizedProduct);
+  if (!isProductValid.result) {
+    return { result: false, message: isProductValid.err };
+  }
+
+  const { product, categoryId } = sanitizedProduct;
+
+  let isProductNameUnique = false;
+  if (product.id) {
+    const isExistingProduct = await prisma.product.findFirst({
+      where: {
+        name: product.name,
+      },
+    });
+    isProductNameUnique = !isExistingProduct
+      ? true
+      : isExistingProduct.id === Number(product.id);
+  } else {
+    isProductNameUnique = Boolean(
+      !(await prisma.product.findFirst({
+        where: {
+          name: product.name,
+        },
+      }))
+    );
+  }
+
+  if (!isProductNameUnique) {
+    return {
+      result: false,
+      message: "This product name already exists",
+    };
+  }
+
+  const isCategoryExist = await prisma.category.findFirst({
+    where: {
+      id: Number(categoryId),
+    },
+  });
+  W;
+  if (!isCategoryExist) {
+    return {
+      result: false,
+      message: "No such an existing category",
+    };
+  }
+  return { result: true, message: "product is validated successfully" };
+}
