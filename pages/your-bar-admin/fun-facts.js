f3b49038c834e949f49cffcb8e3620b6e527cc09
zhzlatkov <zhzlatkov@gmail.com>
2023-08-01 22:12:02 +0300

    feat: new page fun-facts

---
+++ pages/your-bar-admin/fun-facts.js
@@ -0,0 +1,25 @@
+import AdminLayout from "@/components/Layouts/AdminLayout";
+import normalizeFunFact from "@/normalizers/frontend/normalizeFunFact";
+import prisma from "../../services/prismaClient.mjs";
+import Sheet from "@/components/Sheet";
+
+export default function FunFacts({ funFacts }) {
+  const sanitizedFunFacts = funFacts.map((funFact) => {
+    return normalizeFunFact(funFact);
+  });
+  return (
+    <AdminLayout current={"funFacts"}>
+      <Sheet sheetName="funFact" data={sanitizedFunFacts}></Sheet>
+    </AdminLayout>
+  );
+}
+
+export async function getServerSideProps() {
+  let funFacts = await prisma.FunFacts.findMany();
+
+  return {
+    props: {
+      funFacts,
+    },
+  };
+}
