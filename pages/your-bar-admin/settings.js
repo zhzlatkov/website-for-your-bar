180c409d2f0ef5dde82c7f377f8df2cc7984c762
zhzlatkov <zhzlatkov@gmail.com>
2023-08-01 22:16:42 +0300

    fix: update settings admin page to include socialMedais and services forms

--- pages/your-bar-admin/settings.js
+++ pages/your-bar-admin/settings.js
@@ -1,25 +1,58 @@
 import AdminLayout from "@/components/Layouts/AdminLayout";
-import normalizeSetting from "@/normalizers/frontend/normalizeSettings";
+import normalizeSettings from "@/normalizers/frontend/normalizeSettings";
+import normalizeService from "@/normalizers/frontend/normalizeService";
+import normalizeSocialMedia from "@/normalizers/frontend/normalizeSocialMedia";
 import prisma from "@/services/prismaClient.mjs";
 import Form from "@/components/Form";
 
-export default function Settings({ settings }) {
-  const sanitizedSettings = normalizeSetting(settings);
+export default function Settings({ settings, socialMedias, services }) {
+  const sanitizedSettings = normalizeSettings(settings);
+  const sanitizedSocialMedias = socialMedias.map((socialMedia) => {
+    return normalizeSocialMedia(socialMedia);
+  });
+  const sanitizedServices = services.map((service) => {
+    return normalizeService(service);
+  });
   return (
     <AdminLayout current={"settings"}>
-      <Form
-        formName={"settings"}
-        destinationURL={"/"}
-        dataObject={sanitizedSettings}
-      />
+      <div>
+        <Form
+          formName={"settings"}
+          destinationURL={"/"}
+          dataObject={sanitizedSettings}
+        />
+        {sanitizedSocialMedias.map((socialMedia) => {
+          return (
+            <>
+              <Form
+                formName={"social_media"}
+                destinationURL={""}
+                dataObject={socialMedia}
+              />
+            </>
+          );
+        })}
+        {sanitizedServices.map((service) => {
+          return (
+            <>
+              <Form
+                formName={"service"}
+                destinationURL={""}
+                dataObject={service}
+              />
+            </>
+          );
+        })}
+      </div>
     </AdminLayout>
   );
 }
 
 export async function getServerSideProps() {
   const settings = await prisma.settings.findFirst({});
-  console.log(settings);
+  const socialMedias = await prisma.socialMedias.findMany({});
+  const services = await prisma.services.findMany({});
   return {
-    props: { settings },
+    props: { settings, socialMedias, services },
   };
 }
