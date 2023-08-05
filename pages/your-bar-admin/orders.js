9ce44ead976503a547c556e1c70068708d908de6
zhzlatkov <zhzlatkov@gmail.com>
2023-08-05 11:51:53 +0300

    feat: new admin page orders

---
+++ pages/your-bar-admin/orders.js
@@ -0,0 +1,27 @@
+import normalizeOrder from "@/normalizers/frontend/normalizeOrder";
+import AdminLayout from "@/components/Layouts/AdminLayout";
+import prisma from "../../services/prismaClient.mjs";
+import Sheet from "@/components/Sheet";
+
+export default function Orders({ orders }) {
+  const sanitizedOrders = orders.map((order) => {
+    return normalizeOrder(order);
+  });
+  return (
+    <AdminLayout current={"orders"}>
+      <Sheet sheetName="order" data={sanitizedOrders}></Sheet>
+    </AdminLayout>
+  );
+}
+
+export async function getServerSideProps() {
+  let orders = await prisma.Orders.findMany({
+    include: { products: true, table: true },
+  });
+
+  return {
+    props: {
+      orders,
+    },
+  };
+}
