a16ef01e04faf9a24c423051584fca0e950ed291
zhzlatkov <zhzlatkov@gmail.com>
2023-08-05 12:07:50 +0300

    feat new API ednpoint fun-facts

---
+++ pages/api/fun-fact.js
@@ -0,0 +1,56 @@
+import prisma from "../../services/prismaClient.mjs";
+import normalizedFunFact from "../../normalizers/backend/normalizedFunFact.js";
+import funFactSchema from "../../schemas/funFactSchema.js";
+import dataValidator from "../../validators/dataValidator.js";
+
+export default async function updatedFunFact(req, res) {
+  if (req.method !== "POST") {
+    return res
+      .status(405)
+      .setHeader("Allow", "POST")
+      .send({ message: "Wrong HTTP Request Method" });
+  }
+
+  let sanitizeddFunFact;
+  try {
+    sanitizeddFunFact = normalizedFunFact(req.body.data);
+  } catch (err) {
+    return res.status(400).send({ message: `${err.message}` });
+  }
+
+  const validateddFunFact = await dataValidator(
+    "funFact",
+    sanitizeddFunFact,
+    funFactSchema
+  );
+
+  if (validateddFunFact.result) {
+    return res.status(422).send({ message: `${validateddFunFact.message}` });
+  }
+
+  try {
+    if (sanitizeddFunFact.hasOwnProperty("id")) {
+      await prisma.funFacts.create({
+        data: {
+          ...sanitizeddFunFact,
+        },
+      });
+    } else {
+      await prisma.funFacts.update({
+        where: {
+          id: sanitizeddFunFact.id,
+        },
+        data: {
+          ...sanitizeddFunFact,
+        },
+      });
+    }
+    return res
+      .status(200)
+      .send({ message: "Successfully created/updated a dFunFact." });
+  } catch (e) {
+    return res.status(400).send({
+      message: `${err.message}`,
+    });
+  }
+}
