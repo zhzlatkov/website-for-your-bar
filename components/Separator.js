8d3a661bd2e961ead049c42ec5393b2b0385b7c4
zhzlatkov <zhzlatkov@gmail.com>
2023-08-16 22:41:08 +0300

    fix: typo in FontAwesomeIcon size

--- components/Separator.js
+++ components/Separator.js
@@ -4,16 +4,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Separator({ withIcon = true }) {
   return (
     <div
-      className={
-        (withIcon ? "-my-3.5 " : "") + "flex w-screen items-center z-10 m-auto"
-      }
+      className={(withIcon ? "-my-3.5 " : "") + "flex items-center z-10 m-auto"}
     >
       <div className="h-1 w-auto m-auto grow bg-gradient-to-r from-pirateGold-500 from-50% to-pirateGold-100"></div>
       {withIcon ? (
         <div className="flex justify-center m-auto items-center bg-pirateGold-500 w-8 h-8 rounded-full border-2 border-pirateGold-100 z-10">
           <FontAwesomeIcon
             icon={faMartiniGlass}
-            size="l"
+            size="lg"
             className="text-pirateGold-100 mx-auto"
           />
         </div>
