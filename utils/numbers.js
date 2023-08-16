11c4f4d4847a29d460fa898076bfa2b9cdd64a31
zhzlatkov <zhzlatkov@gmail.com>
2023-08-16 22:49:33 +0300

    feat: add utility global const numbers

---
+++ utils/numbers.js
@@ -0,0 +1,7 @@
+export const SECOND_IN_MS = 1000;
+export const MINUTE_IN_MS = SECOND_IN_MS * 60;
+export const HOUR_IN_MS = MINUTE_IN_MS * 60;
+export const DAY_IN_MS = HOUR_IN_MS * 24;
+export const WEEK_IN_MS = DAY_IN_MS * 7;
+export const MONTH_IN_MS = DAY_IN_MS * 30;
+export const YEAR_IN_MS = DAY_IN_MS * 365;
