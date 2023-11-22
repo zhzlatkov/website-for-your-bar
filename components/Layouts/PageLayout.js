3bb4d0bd9dead4ffcc9816ac467ed26c621fb358
zhzlatkov <zhzlatkov@gmail.com>
2023-11-22 23:31:20 +0200

    feat: vercel analytics

--- components/Layouts/PageLayout.js
+++ components/Layouts/PageLayout.js
@@ -4,11 +4,12 @@ import Separator from "@/components/Separator.js";
 import ScrollToTop from "@/components/ScrollToTop.js";
 import AllowOrdering from "@/components/AllowOrdering.js";
 
+import { Analytics } from "@vercel/analytics/react";
+
 export default function PageLayout({ children, settings, socialMedias }) {
   return (
     <div className="flex bg-shark-950 flex-col min-h-screen">
       <Navbar settings={settings} />
-
       <AllowOrdering />
       <main className="flex-grow bg-shark-950 h-full">
         <ScrollToTop />
@@ -16,6 +17,7 @@ export default function PageLayout({ children, settings, socialMedias }) {
       </main>
       <Separator withIcon={false} />
       <Footer socialMedias={socialMedias} />
+      <Analytics />
     </div>
   );
 }
